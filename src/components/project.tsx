6aab6ab61deec9fa7ab3f1b8f2cd5c65e9de4972
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-09-09 08:01:21 +0530

    add link to project item

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -11,7 +11,8 @@ const Project = (projectUrl: ProjectUrl) => {
     });
     
   return (
-    <article className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden max-h-[20rem] sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-lg">
+    <article className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden max-h-[20rem] sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-lg"
+        onClick={() => window.open(project?.githubUrl, '_blank')}>
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{project?.name}</h3>
             <p className="mt-2 leading-relaxed text-gray-700 sm:h-1/2">{project?.description}</p>
